5b602f6386ed729a741e538546bd7d3d73fa1ca5
mzacki <38253922+mzacki@users.noreply.github.com>
2022-09-29 20:51:50 +0200

    lesson 1 - strings, variables, values

---
+++ learning/lesson_1.py
@@ -0,0 +1,21 @@
+# function to reverse string starting from last index (-1)
+def reverse(s):
+    return s[::-1]
+
+
+# assignment to a variable (cokolwiek), value is "cokolwiek"
+cokolwiek = "cokolwiek"
+# tabela / array - example:
+# [1, 2, 3, 4]
+
+# get value (character) by index (here: 7)
+# dowolna_litera = cokolwiek[7]
+
+# print result of function cokolwiek
+print(cokolwiek[-2])
+
+# print(reverse(cokolwiek))
+
+# anything = "Anything"
+
+# print(anything)
