3862d9863752dd9e06a5d0ca4c0ed907a6ad8b49
mzacki <38253922+mzacki@users.noreply.github.com>
2022-10-04 19:03:25 +0200

    lesson 3 - tasks

---
+++ learning/lesson_3.py
@@ -0,0 +1,14 @@
+# TODO na podstawie materiałów z lekcji 1 i 2 :
+# Zadanie 1
+# Podnieś 2 do potęgi 8, wynik przypisz do zmiennej "result" i wypisz do konsoli
+# Oblicz i wypisz do konsoli (print) pierwiastek kwadratowy (square root) z liczby 64 po przypisaniu do zmiennej "result 2"
+# Oblicz pierwiastek trzeciego stopnia (cube root) z liczby 64 i wypisz do konsoli bez przypisania do zmiennej
+# Oblicz pierwiastek czwartego stopnia (quad root) z liczby 64 i jak wyżej
+
+# Zadanie 2
+# Wypisz do konsoli swoje imię 1028 razy
+
+# *Zadanie 3
+# Ze stringa "Instalacje fotowoltaiczne i elektryczne" wypisz do konsoli tylko te litery (znaki),
+# których index jest liczbą nieparzystą
+
